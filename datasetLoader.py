903f9c32998db97f22bf7d95bc2acc46d3970c6b
adrie <1750927@uab.cat>
2025-11-08 13:42:11 +0100

    x

--- datasetLoader.py
+++ datasetLoader.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from stations import BusStation
 
 bus_stops_df = pd.read_excel("\dataset\Datasets Barcelona\Parades Bus Barcelona.xlsx")
+bus_stations = list()
 
 for line in bus_stops_df:
+    bus_stations.append(BusStation(code=, name=, coord=))
