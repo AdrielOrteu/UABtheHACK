a5fbddd4e864fdaf321720f400810206bcb6ca26
adrie <1750927@uab.cat>
2025-11-08 16:55:46 +0100

    v2

--- datasetLoader.py
+++ datasetLoader.py
@@ -1,22 +1,27 @@
 import pandas as pd
 from stations import BusStation
 import networkx
+def load_bus_stations():
+    bs_df = pd.read_excel("dataset\Datasets Barcelona\Parades Bus Barcelona.xlsx")
+    selected_columns = bs_df[['Código de parada / Codi de parada', 'Nombre / Nom', 'UTM X', 'UTM Y']]
+    tuples_list = list(selected_columns.itertuples(index=False, name=None))
+    
+    # Convert 'UTM X' and 'UTM Y' from string with ',' decimal separator to float
+    selected_columns['UTM X'] = selected_columns['UTM X'].astype(str).str.replace(',', '.').astype(float)
+    selected_columns['UTM Y'] = selected_columns['UTM Y'].astype(str).str.replace(',', '.').astype(float)
+    
+    # Convert to list of tuples
+    tuples_list = list(selected_columns.itertuples(index=False, name=None))
+    
+    bus_stations = list()
+    
+    for value in tuples_list:
+        bus_stations.append(BusStation(code=value[0], name=value[1], coord_x=value[2], coord_y=value[3]))
+    
+    for s in bus_stations:
+        print(s)
 
-bs_df = pd.read_excel("dataset\Datasets Barcelona\Parades Bus Barcelona.xlsx")
-selected_columns = bs_df[['Código de parada / Codi de parada', 'Nombre / Nom', 'UTM X', 'UTM Y']]
-tuples_list = list(selected_columns.itertuples(index=False, name=None))
 
-# Convert 'UTM X' and 'UTM Y' from string with ',' decimal separator to float
-selected_columns['UTM X'] = selected_columns['UTM X'].astype(str).str.replace(',', '.').astype(float)
-selected_columns['UTM Y'] = selected_columns['UTM Y'].astype(str).str.replace(',', '.').astype(float)
-
-# Convert to list of tuples
-tuples_list = list(selected_columns.itertuples(index=False, name=None))
-
-bus_stations = list()
-
-for value in tuples_list:
-    bus_stations.append(BusStation(code=value[0], name=value[1], coord_x=value[2], coord_y=value[3]))
-
-for s in bus_stations:
-    print(s)
+def load_networks():
+    sorted_lines = pd.read_csv("dataset\Datasets Barcelona\ggg.csv")
+    
